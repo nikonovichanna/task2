d927a1076eccf065df0162be34a9ec4387f59690
nikonovichanna <nikonovicvhhanna@gmail.com>
2023-08-07 00:24:10 +0300

    add task2

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿﻿int numberA = new Random().Next(1,20);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,20);
+Console.WriteLine(numberB);
+int numberC = new Random().Next(1,20);
+Console.WriteLine(numberC);
+int max = numberA;
+if (numberB > max)
+{
+    max = numberB;
+}
+if (numberC > max)
+{
+    max = numberC;
+}
+System.Console.WriteLine(max);
+
